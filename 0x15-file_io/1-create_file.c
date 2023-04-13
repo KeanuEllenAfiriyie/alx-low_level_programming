1fd8a70b7dfcbda12c41463b6f5484836db480ff
KeanuEllenAfiriyie <122979498+KeanuEllenAfiriyie@users.noreply.github.com>
2023-04-13 16:19:24 +0500

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,13 +4,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-/**
- * create_file - creates a file.
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
- *
- * Return: -1 if failure else 1
- */
 
 int create_file(const char *filename, char *text_content)
 {
